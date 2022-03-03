c976409291c4e9e762ceeca28173cca8fd79dfb8
Julio Grecco <jagrecco@gmail.com>
2022-03-02 23:49:29 -0300

    ID en las fichas ID

--- js/codigo.js
+++ js/codigo.js
@@ -1,5 +1,8 @@
 const tablero=[];
+const ficherio=[];
 let colorCasillero=0;
+let nombrePlayer;
+let colorPlayer;
 class Celda{
 
     constructor(ubicacion,color,estado)
@@ -11,8 +14,9 @@ class Celda{
 }
 class Ficha{
     
-    constructor(color, posicion, viva, dama)
+    constructor(id, color, posicion, viva, dama)
     {
+        this.id=id;
         this.color = color;
         this.posicion = posicion;
 	    this.viva = viva;
@@ -31,41 +35,11 @@ class Ficha{
 }
 
 TableroArray();
-
-prueba();
-
-/*
-for (let i=0; i<64; i++){
-
-    
-    if (((i%8)!==0) && i!==0)
-    {
-        if (colorCasillero===0)
-        {
-            colorCasillero=1;
-        } else
-        {
-            colorCasillero=0;
-        }
-    }
-    
-    Casilla=new Celda(i,colorCasillero,0);
-
-    tablero.push(Casilla);
-
-}
-
-tableroHtml=document.getElementById("tablero");
-tableroHtml.innerHTML ='<div class="item celda__b"></div>';
-
-console.log(tablero);
-*/
-
+FichasArray();
 
 function TableroArray(){
-
+    
     for (let i=0; i<64; i++){
-        
         
         if (((i%8)!==0) && i!==0)
         {
@@ -81,50 +55,109 @@ function TableroArray(){
         Casilla=new Celda(i,colorCasillero,0);
         
         tablero.push(Casilla);
-        console.log(Casilla);
-        
     }
 }
 
+function IniciaJuego()
+{
+    Jugador();
+    ArmaTablero();
+}
 
-function prueba()
+function Jugador()
+{
+    nombrePlayer=prompt("Ingrese su nombre");
+    
+    jugador_nombre=document.getElementById("playerNombre");
+    jugador_nombre.innerHTML="Jugador = " + nombrePlayer;
+
+}
+
+function FichasArray()
+{
+    blancas= new Ficha(0,0,1,1,0);
+    ficherio.push(blancas);
+    blancas= new Ficha(1,0,3,1,0);
+    ficherio.push(blancas);
+    blancas= new Ficha(2,0,5,1,0);
+    ficherio.push(blancas);
+    blancas= new Ficha(3,0,7,1,0);
+    ficherio.push(blancas);
+    blancas= new Ficha(4,0,8,1,0);
+    ficherio.push(blancas);
+    blancas= new Ficha(5,0,10,1,0);
+    ficherio.push(blancas);
+    blancas= new Ficha(6,0,12,1,0);
+    ficherio.push(blancas);
+    blancas= new Ficha(7,0,14,1,0);
+    ficherio.push(blancas);
+    blancas= new Ficha(8,0,17,1,0);
+    ficherio.push(blancas);
+    blancas= new Ficha(9,0,19,1,0);
+    ficherio.push(blancas);
+    blancas= new Ficha(10,0,21,1,0);
+    ficherio.push(blancas);
+    blancas= new Ficha(11,0,23,1,0);
+    ficherio.push(blancas);
+
+    rojas= new Ficha(12,1,40,1,0);
+    ficherio.push(rojas);
+    rojas= new Ficha(13,1,42,1,0);
+    ficherio.push(rojas);
+    rojas= new Ficha(14,1,44,1,0);
+    ficherio.push(rojas);
+    rojas= new Ficha(15,1,46,1,0);
+    ficherio.push(rojas);
+    rojas= new Ficha(16,1,49,1,0);
+    ficherio.push(rojas);
+    rojas= new Ficha(17,1,51,1,0);
+    ficherio.push(rojas);
+    rojas= new Ficha(18,1,53,1,0);
+    ficherio.push(rojas);
+    rojas= new Ficha(19,1,55,1,0);
+    ficherio.push(rojas);
+    rojas= new Ficha(20,1,56,1,0);
+    ficherio.push(rojas);
+    rojas= new Ficha(21,1,58,1,0);
+    ficherio.push(rojas);
+    rojas= new Ficha(22,1,60,1,0);
+    ficherio.push(rojas);
+    rojas= new Ficha(23,1,62,1,0);
+    ficherio.push(rojas);
+}
+
+
+function ArmaTablero()
+
 {
     tableroHtml=document.getElementById("tablero");
     let dibujo="";
 
-    tablero.forEach(element=> console.log(element.color));
-
-    tablero.forEach(element=> {
+    tablero.forEach(element=>
+        {
             if (element.color==0)
             {
-                dibujo=dibujo + '<div class="item celda__w"></div>';    
+                dibujo=dibujo + `<div class="item celda__w">${element.ubicacion}</div>`; /*  pone ubicación en celdas blancas ${element.ubicacion}  */
+                
             } else
             {
-                dibujo=dibujo + '<div class="item celda__b"></div>';
+                if (element.ubicacion>=0 && element.ubicacion<=23)
+                {
+                    dibujo=dibujo + `<div class="item celda__b"><div class="ficha__b" id="${element.ubicacion}" onclick="muestraID()"></div></div>`;
+                } else if (element.ubicacion>=40 && element.ubicacion<=63)
+                {
+                    dibujo=dibujo + '<div class="item celda__b"><div class="ficha__r"></div></div>';
+                } else
+                {
+                    dibujo=dibujo + '<div class="item celda__b"></div>';
+                }
             }
 
             tableroHtml.innerHTML = dibujo;
         });
 }
 
-function ArmaTablero()
+function muestraID()
 {
-    tableroHtml=document.getElementById("tablero");
-
-    let dibujo="";
-
-    for (let i=0; i<64; i++)
-    {
-        if (tablero[i].Celda.color==0)
-        {
-            dibujo=dibujo + '<div class="item celda__w"></div>';
-        } else 
-        {
-            dibujo=dibujo + '<div class="item celda__b"></div>';
-        }
-    }
-    console.log(dibujo);
-    tableroHtml.innerHTML = dibujo;
     
 }
-
